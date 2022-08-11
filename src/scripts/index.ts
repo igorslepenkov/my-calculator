7e69ebbed1cc55682d0fdb8660a402a2f64fa4e6
igorslepenkov <slepenkov.nii@yandex.by>
2022-08-11 14:56:24 +0300

    feat(new_release): implement create function

---
+++ src/scripts/index.ts
@@ -0,0 +1,12 @@
+import { Calculator } from "./components/Calculator.js";
+
+const initApp = () => {
+  const root = document.querySelector("#root");
+
+  if (root) {
+    const calculator = new Calculator(root);
+    calculator.create();
+  }
+};
+
+initApp();
